f39d787da795a6e18e6d120b3da14089c75b119e
Eduardo Hobold <eduardo.hobold1@gmail.com>
2024-04-11 19:38:39 -0300

    Concluindo projeto do DT Money

---
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -0,0 +1,64 @@
+import { MagnifyingGlass } from 'phosphor-react'
+import { SearchFormContainer } from './styles'
+import { useForm } from 'react-hook-form'
+import * as z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { TransactionsContext } from '../../../../contexts/TransactionsContext'
+import { useContextSelector } from 'use-context-selector'
+
+/*
+  Porque que um componente redenriza?
+
+    Hooks changed (mudou estado, mudou contexto, reducer);
+    Props changed (mudou propriedades);
+    Parent rerendered (componente pai renderizou);
+  
+  Qual o fluxo de renderização? (EXTREMAMENTE RÁPIDO)
+    1. React recria o HTML da interface daquele componente
+    2. Compara a versão do HTML recriada com a versão anterior
+    3. Se mudou algo, reescreve o HTML em tela
+
+  Memo:
+    0. Hooks changed, Props changed (deep comparison)
+    0.1 Comparar com a versão anterior dos hooks e props
+    0.2 Se mudou algo, ele vai permitir a nova renderização
+*/
+
+const searchFormSchema = z.object({
+  query: z.string(),
+})
+
+type SearchFormInputs = z.infer<typeof searchFormSchema>
+
+export function SearchForm() {
+  const fetchTransactions = useContextSelector(TransactionsContext, (context) => {
+    return context.fetchTransactions;
+  })
+
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SearchFormInputs>({
+    resolver: zodResolver(searchFormSchema),
+  })
+
+  async function handleSearchTransactions(data: SearchFormInputs) {
+    await fetchTransactions(data.query)
+  }
+
+  return (
+    <SearchFormContainer onSubmit={handleSubmit(handleSearchTransactions)}>
+      <input
+        type="text"
+        placeholder="Busque por transações"
+        {...register('query')}
+      />
+
+      <button type="submit" disabled={isSubmitting}>
+        <MagnifyingGlass size={20} />
+        Buscar
+      </button>
+    </SearchFormContainer>
+  )
+}
